919e9941f593673d0aefcaa9d26390d829a12ea8
David Acquah <scherryprince@gmail.com>
2023-04-19 15:30:36 +0000

    group assignment

---
+++ script.js
@@ -0,0 +1,18 @@
+
+        /*js for showing the main dropdown*/
+    function show_dropdown(){
+        document.getElementById("drop").style.display="block"
+    }
+    /*js for showing the dropdown under the service */
+    function show_menu(){
+        document.getElementById("dropdown").style.display="block"
+    }
+    /*js for hidding the main dropdown*/
+    function hide_dropdown(){
+        document.getElementById("drop").style.display="none"
+    }
+    /*script for hiding the */
+    function hide_menu(){
+        document.getElementById("dropdown").style.display="none"
+    }
+  
